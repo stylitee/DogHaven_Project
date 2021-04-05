3da638fbd78d2d855cd30eaff485908a96f98b06
Christian Balondo <balondochristian22@gmail.com>
2021-04-05 16:28:23 +0800

    inserted login and registration page

---
+++ doghavenCapstone/doghavenCapstone/InitialPages/LoginPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace doghavenCapstone.InitialPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ doghavenCapstone/doghavenCapstone/InitialPages/Register.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace doghavenCapstone.InitialPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Register : ContentPage
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
