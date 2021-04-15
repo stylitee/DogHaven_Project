83f8812b6fc017b919fbf8eddd3d8b182c91551d
Christian Balondo <balondochristian22@gmail.com>
2021-04-15 23:29:51 +0800

    worked on LostDogs code behind because im repeating it

--- doghavenCapstone/doghavenCapstone/TabbedPageParts/LostPage.xaml.cs
+++ doghavenCapstone/doghavenCapstone/TabbedPageParts/LostPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using doghavenCapstone.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,30 @@ namespace doghavenCapstone.TabbedPageParts
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LostPage : ContentPage
     {
+        public ObservableCollection<LostDogs> _LostDoglist = new ObservableCollection<LostDogs>();
         public LostPage()
         {
             InitializeComponent();
+            BindingContext = this;
+        }
+
+        public async void LoadLostDogs()
+        {
+            var LostList = await App.client.GetTable<LostDogs>().ToListAsync();
+
+            foreach(var c in LostList)
+            {
+                //displaying of data in list view
+            }
+        }
+
+        public ObservableCollection<LostDogs> LostDoglist
+        {
+            get => _LostDoglist;
+            set
+            {
+                _LostDoglist = value;
+            }
         }
     }
 }
